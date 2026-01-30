55055d107b7d48b98e71b299bced0b4ed61cda11
we-BAK <hailemichaelkidist@gmail.com>
2026-01-30 22:42:58 +0300

    Simplify subset check using Counter comparison

--- Array_subset.py
+++ Array_subset.py
@@ -2,12 +2,4 @@ from collections import Counter
 
 class Solution:
     def isSubset(self, a, b):
-        pantry = Counter(a)
-        
-        for ingredient in b:
-            if pantry[ingredient] > 0:
-                pantry[ingredient] -= 1
-            else:
-                return False
-        
-        return True
+        return Counter(b) <= Counter(a)
