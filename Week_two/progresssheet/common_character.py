528638ad21056ac26543b18faf36ee18209e386c
we-BAK <hailemichaelkidist@gmail.com>
2026-02-09 14:47:44 +0300

    solution for two problems

---
+++ Week_two/progresssheet/common_character.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        common = Counter(words[0])
+
+        for word in words[1:]:
+            common &= Counter(word)
+
+        return list(common.elements())
