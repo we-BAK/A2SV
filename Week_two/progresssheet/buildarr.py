5917a12060b7765348b31961e90a49b7dd64a495
we-BAK <hailemichaelkidist@gmail.com>
2026-02-10 17:14:12 +0300

    buildarr

---
+++ Week_two/progresssheet/buildarr.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        result=[]
+        for i in range(len(nums)):
+            result.append(nums[nums[i]])
+        return result
+
+        
+        
